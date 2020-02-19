da0f70fb14c5ce5e619a27a46aeab2cb2daecd2f
cdunn1002 <60614914+cdunn1002@users.noreply.github.com>
2020-02-19 10:24:06 -0600

    Turrets
    Added functional turrets.

---
+++ Barry Draper/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,36 @@
+/*****************************************************************************
+// File Name : BulletScript.cs
+// Author : Connor Dunn
+// Creation Date : February 19, 2020
+//
+// Brief Description : Bullets fly forward and impact the player.
+*****************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    public float bulletSpeed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * bulletSpeed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+            GameControllerScript.playerLives--;
+        }
+    }
+}
---
+++ Barry Draper/Assets/Scripts/TurretScript.cs
@@ -0,0 +1,55 @@
+/*****************************************************************************
+// File Name : TurretScript.cs
+// Author : Connor Dunn
+// Creation Date : February 17, 2020
+//
+// Brief Description :  A functional turret that activates when the player 
+//                      is in range, and follows the player while shooting 
+//                      bullets.
+*****************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretScript : MonoBehaviour
+{
+    public GameObject bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Vector3 playerPosition = collision.gameObject.transform.position;
+            Vector2 delta = transform.position - playerPosition;
+
+            float angle = Mathf.Atan2(delta.y, delta.x) * Mathf.Rad2Deg;
+
+            Quaternion rot = Quaternion.Euler(new Vector3(0, 0, angle));
+            transform.rotation = rot;
+            Invoke("shoot", 2f);
+        }
+    }
+
+    public float timeBetweenShots = 1f;
+    private float validShotTime;
+    void shoot()
+    {
+        if(Time.time >= validShotTime)
+        {
+            Instantiate(bullet, transform.position, transform.rotation);
+            validShotTime = Time.time + timeBetweenShots;
+        }
+        
+    }
+}
