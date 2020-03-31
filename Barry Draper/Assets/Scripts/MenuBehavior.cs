a2368aee17a6b86aae23c89852e6f62275f7b748
RobertLee-bradley <58155031+RobertLee-bradley@users.noreply.github.com>
2020-03-31 15:30:36 -0500

    Main Menu Changes
    Added some WIP Main Menu Changes

--- Barry Draper/Assets/Scripts/MenuBehavior.cs
+++ Barry Draper/Assets/Scripts/MenuBehavior.cs
@@ -10,18 +10,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuBehavior : MonoBehaviour
 {
     public GameObject titleScreen;
     public GameObject levelSelect;
     public GameObject levelOne;
+    public GameObject levelTwo;
+    public GameObject levelThree;
+    public GameObject LevelFour;
+    public Button LevelTwoButton;
+    public Button LevelThreeButton;
+    public Button LevelFourButton;
+
+    public static float ProgressCheck = 0.5f;
 
     private void Start()
     {
         titleScreen.SetActive(true);
         levelSelect.SetActive(false);
         levelOne.SetActive(false);
+        levelTwo.SetActive(false);
+        levelThree.SetActive(false);
+        LevelFour.SetActive(false);
+
+        if (ProgressCheck < 1)
+        {
+            Object.Destroy(LevelTwoButton);
+            Object.Destroy(LevelThreeButton);
+            Object.Destroy(LevelFourButton);
+        }
+        else if (ProgressCheck < 2)
+        {
+            Object.Destroy(LevelThreeButton);
+            Object.Destroy(LevelFourButton);
+        }
+        else if (ProgressCheck < 3)
+        {
+            Object.Destroy(LevelFourButton);
+        }
     }
 
     /// <summary>
